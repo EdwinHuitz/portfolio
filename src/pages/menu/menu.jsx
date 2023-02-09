66989edd06327a7826fa4e7ee237d2e811bdd71b
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2023-02-09 17:34:27 -0500

    added lots of content to menu demo page and moved images from menu folder to public folder

--- src/pages/menu/menu.jsx
+++ src/pages/menu/menu.jsx
@@ -5,10 +5,23 @@ export default function Menu(){
    //let arr=["pancakes","waffles","omelette","biscuits & gravy","eggs benedict","hashbrown","loaded fries","bacon cheeseburger","reuben","milkshake","iced-tea","coffee"];
    return(
    <>
-      <div className="menuNav">
-         <h1 className="title">Sunny's Diner</h1>
-         <h3 className="tabs"><button>All</button><button>Breakfast</button><button>Lunch</button><button>Drinks</button>
-         </h3>
+      <div className="menuWrap">
+         <div className="menuNav">
+            <h1 className="title">Sunny's Diner</h1>
+            <hr/>
+            <h3 className="tabs"><button>All</button><button>Breakfast</button><button>Lunch</button><button>Drinks</button>
+            </h3>
+         </div>
+         <div className="menuList">
+            <div className="menuItem">
+               <img src="assets/img/food/pancakes.jpg" alt="" className="foodPic"></img>
+               <div className="foodDetail">
+                  <span className="foodText"><b>Pancakes</b> <b className="foodPrice">$15</b></span>
+                  <hr></hr>
+                  <p>Pancakes are made of pancake mix which is made of pancake mix which is made of pancake mix which is made of pancake mix.</p>
+               </div>
+            </div>
+         </div>
       </div>
    </>
    )
