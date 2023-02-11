02c25ce20101760a7208ed7091eb610f30f62f61
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2023-02-11 03:49:46 -0500

    added menu items and updated css to be more desktop and mobile friendly

--- src/pages/menu/menu.jsx
+++ src/pages/menu/menu.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import './menu.css'
+
 export default function Menu(){
+
+   let arr=[[0,"Pancakes"],[0,"Waffles"],[0,"Omelette"],[0,"Biscuits and Gravy"],[0,"Eggs Benedict"],[0,"Hashbrown"],[1,"Loaded Fries"],[1,"Bacon Cheeseburger"],[1,"Reuben Sandwich"],[2,"Milkshake"],[2,"Iced-Tea"],[2,"Coffee"]];
+   let path="assets/img/food/"
+   function menuItems(foodType,foodName){
+      let newPath = path+foodName+".jpg"
+         return (<>
+               <div className="menuItem" id={foodType}>
+                     <img src={newPath} alt="" className="foodPic"></img>
+                     <div className="foodDetail">
+                        <span className="foodText"><b>{foodName}</b> <b className="foodPrice">$15</b></span>
+                        <hr></hr>
+                        <p>{foodName} are made of {foodName} mix which is made of {foodName} mix which is made of {foodName} mix which is made of {foodName}!</p>
+                     </div>
+               </div>
+      </>)
+   
+   }
    //!restaurant menu similar to this page https://react-projects-5-menu.netlify.app/
-   //let arr=["pancakes","waffles","omelette","biscuits & gravy","eggs benedict","hashbrown","loaded fries","bacon cheeseburger","reuben","milkshake","iced-tea","coffee"];
    return(
    <>
       <div className="menuWrap">
@@ -13,14 +30,18 @@ export default function Menu(){
             </h3>
          </div>
          <div className="menuList">
-            <div className="menuItem">
-               <img src="assets/img/food/pancakes.jpg" alt="" className="foodPic"></img>
-               <div className="foodDetail">
-                  <span className="foodText"><b>Pancakes</b> <b className="foodPrice">$15</b></span>
-                  <hr></hr>
-                  <p>Pancakes are made of pancake mix which is made of pancake mix which is made of pancake mix which is made of pancake mix.</p>
-               </div>
-            </div>
+            {menuItems(arr[0][0],arr[0][1])}
+            {menuItems(arr[1][0],arr[1][1])}
+            {menuItems(arr[2][0],arr[2][1])}
+            {menuItems(arr[3][0],arr[3][1])}
+            {menuItems(arr[4][0],arr[4][1])}
+            {/* {menuItems(arr[5][0],arr[5][1])}
+            {menuItems(arr[6][0],arr[6][1])}
+            {menuItems(arr[7][0],arr[7][1])}
+            {menuItems(arr[8][0],arr[8][1])}
+            {menuItems(arr[9][0],arr[9][1])}
+            {menuItems(arr[10][0],arr[10][1])}
+            {menuItems(arr[11][0],arr[11][1])} */}
          </div>
       </div>
    </>
