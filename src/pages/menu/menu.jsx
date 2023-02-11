67fe52f10768c72c86c6d50af4cc26f04dddda3d
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2023-02-11 04:21:57 -0500

    renamed images and updated code to find all images

--- src/pages/menu/menu.jsx
+++ src/pages/menu/menu.jsx
@@ -3,10 +3,18 @@ import './menu.css'
 
 export default function Menu(){
 
-   let arr=[[0,"Pancakes"],[0,"Waffles"],[0,"Omelette"],[0,"Biscuits and Gravy"],[0,"Eggs Benedict"],[0,"Hashbrown"],[1,"Loaded Fries"],[1,"Bacon Cheeseburger"],[1,"Reuben Sandwich"],[2,"Milkshake"],[2,"Iced-Tea"],[2,"Coffee"]];
+   let arr=[[0,"Pancakes"],[0,"Waffles"],[0,"Omelette"],[0,"Biscuits & Gravy"],[0,"Eggs Benedict"],[0,"Hashbrown"],[1,"Loaded Fries"],[1,"Bacon Cheeseburger"],[1,"Reuben Sandwich"],[2,"Milkshake"],[2,"Iced-Tea"],[2,"Coffee"]];
    let path="assets/img/food/"
    function menuItems(foodType,foodName){
-      let newPath = path+foodName+".jpg"
+      let newName=''
+      let newPath=''
+      if(foodName.indexOf(' ')>=0){
+         newName=foodName.replaceAll(' ','-')
+         newPath = path+newName+".jpg"
+      }
+      else{
+         newPath = path+foodName+".jpg"
+      }
          return (<>
                <div className="menuItem" id={foodType}>
                      <img src={newPath} alt="" className="foodPic"></img>
@@ -35,13 +43,13 @@ export default function Menu(){
             {menuItems(arr[2][0],arr[2][1])}
             {menuItems(arr[3][0],arr[3][1])}
             {menuItems(arr[4][0],arr[4][1])}
-            {/* {menuItems(arr[5][0],arr[5][1])}
+            {menuItems(arr[5][0],arr[5][1])}
             {menuItems(arr[6][0],arr[6][1])}
             {menuItems(arr[7][0],arr[7][1])}
             {menuItems(arr[8][0],arr[8][1])}
             {menuItems(arr[9][0],arr[9][1])}
             {menuItems(arr[10][0],arr[10][1])}
-            {menuItems(arr[11][0],arr[11][1])} */}
+            {menuItems(arr[11][0],arr[11][1])}
          </div>
       </div>
    </>
