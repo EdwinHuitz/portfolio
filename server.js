dc19b581aecab59d5351c7b4d71a0a70fecabd56
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2023-02-12 04:09:55 -0500

    updated server.js and package file for aws compatibility

--- server.js
+++ server.js
@@ -1,18 +1,48 @@
-require('dotenv').config()
+// require('dotenv').config()
 const express = require('express')
 const app = express()
-const fs = require('fs')
 const cors = require('cors')
 const logger = require('morgan')
 const http = require('http')
-const https = require('https')
-const cCert = fs.readFileSync('cert.txt','utf8')
-const cKey = fs.readFileSync('key.txt','utf8')
-const credentials={key:cKey,cert:cCert}
-const httpsServer = https.createServer(credentials,app)
+// const fs = require('fs')
+// const https = require('https')
+// const cCert = fs.readFileSync('cert.txt','utf8')
+// const cKey = fs.readFileSync('key.txt','utf8')
+// const credentials={key:cKey,cert:cCert}
+// const httpsServer = https.createServer(credentials,app)
+const httpServer = http.createServer(app)
 const port= 9001
 const devMode=true
 
+import {
+   SecretsManagerClient,
+   GetSecretValueCommand,
+ } from "@aws-sdk/client-secrets-manager";
+ 
+ const secret_name = "portfolio_env";
+ 
+ const client = new SecretsManagerClient({
+   region: "us-east-1",
+ });
+ 
+ let response;
+ 
+ try {
+   response = await client.send(
+     new GetSecretValueCommand({
+       SecretId: secret_name,
+       VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+     })
+   );
+ } catch (error) {
+   // For a list of exceptions thrown, see
+   // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+   throw error;
+ }
+ 
+ const secret = response.SecretString;
+ 
+ // Your code goes here
 
 //!route paths
 const apiRouter=require('./routes/api')
@@ -30,4 +60,5 @@ app.use('/api', apiRouter)
 
 devMode===true?
 app.listen(port,()=>console.log('DEV MODE: listening on port '+port)):
-httpsServer.listen(port,()=>console.log('listening on port '+port))
+httpServer.listen(port,()=>'')
+// httpsServer.listen(port,()=>console.log('listening on port '+port))
