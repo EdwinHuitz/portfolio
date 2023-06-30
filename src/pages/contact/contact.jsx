bb9784dd197043df24b0075a9f69ff9ef6e95203
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2023-06-30 13:50:45 -0500

    updated map and margin for map

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -48,7 +48,7 @@ export default function Contact(){
                      <i className="bx bx-phone"></i>
                      <h5>(<span className="hiddenPhone">01</span><span>2</span><span className="hiddenPhone">01</span><span>4</span><span className="hiddenPhone">02</span><span>0</span><span className="hiddenPhone">03</span>) - <span>7</span><span className="hiddenPhone">05</span><span>2</span><span className="hiddenPhone">08</span><span>2</span><span className="hiddenPhone">13</span> - <span>8</span><span className="hiddenPhone">21</span><span>7</span><span className="hiddenPhone">34</span><span>7</span><span className="hiddenPhone">55</span><span>1</span><span className="hiddenPhone">89</span></h5>
                   </div>
-                  <iframe title="Map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d397483.9561786603!2d-77.29476234483174!3d38.893261661226475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7c6de5af6e45b%3A0xd6e28ec00254a198!2sDistrict%20of%20Columbia!5e0!3m2!1sen!2sus!4v1603425816092!5m2!1sen!2sus>" style={{border:"0", borderRadius:"5px", width: "100%", height: "30vh"}} allowFullScreen></iframe>
+                  <iframe title="Map" src="https://maps.google.com/maps?hl=en&amp;q=%20chicago+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" style={{border:"0", borderRadius:"15px", width: "100%", height: "30vh",marginTop:"10%"}} allowFullScreen></iframe>
                </div>
             </div>
             <div id="contact_box" className="col">
