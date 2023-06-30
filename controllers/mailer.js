83fbe6abada094b9faf356014053b4a4f3eb5d57
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2023-06-29 19:50:16 -0500

    debugging mailer

--- controllers/mailer.js
+++ controllers/mailer.js
@@ -1,18 +1,22 @@
 const mailer=require('sendmail')
 
 async function sendMail(req,res){
-   let r=req.params.mail
-   const sendIt=mailer()
-   let arr=r.split(',')
+   // let r=req.params.mail
+   // const sendIt=mailer()
+   // let arr=r.split(',')
+   // try{
+   //    sendIt({
+   //    from:'contact@edwinhuitz.com',
+   //    to:'edwinhuitz@gmail.com',
+   //    subject:`${arr[0]}:${arr[2]}`,
+   //    html:`${arr[1]}<br/>${arr[3]}`
+   //    })
+   //    res.status(200).send('done!')
+   // }
+   // catch(err){res.send(err)}
    try{
-      sendIt({
-      from:'contact@edwinhuitz.xyz',
-      to:'edwinhuitz@gmail.com',
-      subject:`${arr[0]}:${arr[2]}`,
-      html:`${arr[1]}<br/>${arr[3]}`
-      })
-      res.status(200).send('done!')
-   }
-   catch(err){res.send(err)}
+      console.log(req)
+      res.send("done!")
+   }catch(err){res.send(err)}
 }
 module.exports=sendMail
