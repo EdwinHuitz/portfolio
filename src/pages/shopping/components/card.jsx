d2361bf8b8721dbd18841820e2f2bca759ffe347
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2023-06-30 23:35:46 -0500

    adding small change to cards

--- src/pages/shopping/components/card.jsx
+++ src/pages/shopping/components/card.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './card.css'
 
-export default function Card(){
+export default function Card(props){
+   let path="assets/img/clothes/"
+   let shirts=["blue-shirt","orange-shirt","salmon-shirt","white-shirt"]
+   let hats=["black-fedora","blue-cap","brown-fedora","grey-cap"]
+   let pants=["brown-pants","folded-pants","jeans","slacks"]
+   let choice=props.i.toLowerCase()==="hats"?hats:props.i.toLowerCase()==="pants"?pants:shirts
    return(
    <>
       <div className="Card">
-         <h1>text</h1>
+         <h3>Newest {props.i}</h3>
+         <img src={path+props.i.toLowerCase()+"/"+choice[0]+".png"} alt="text"></img>
+         <img src={path+props.i.toLowerCase()+"/"+choice[1]+".png"} alt="text"></img><br/>
+         <img src={path+props.i.toLowerCase()+"/"+choice[2]+".png"} alt="text"></img>
+         <img src={path+props.i.toLowerCase()+"/"+choice[3]+".png"} alt="text"></img>
       </div>
    </>)
 }
