d6fa2ff7bde6d2e5a81fd100aa422c6a55fb28a6
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2023-07-03 03:51:15 -0500

    updating styling for shopping and card pages and adding links for shopping page

--- src/pages/shopping/shopping.jsx
+++ src/pages/shopping/shopping.jsx
@@ -10,8 +10,11 @@ export default function Shopping(){
          <nav className="navHead">
             <h4 className="brand">Fashion Front</h4>
             <h4 className="links">Hats</h4>
+            <h4 className="links">Scarves</h4>
             <h4 className="links">Shirts</h4>
+            <h4 className="links">Belts</h4>
             <h4 className="links">Pants</h4>
+            <h4 className="links">Shoes</h4>
             <i className="bx bx-cart shoppingCart"></i>
          </nav>
          <div className="mainDisplay">
