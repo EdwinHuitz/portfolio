d1b862ad2926a3bb8ed0233d3c6222454a5b2325
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2023-07-03 20:09:35 -0500

    updating detailed card styling

--- src/pages/shopping/components/card.jsx
+++ src/pages/shopping/components/card.jsx
@@ -10,24 +10,18 @@ let prices=[14.75,12.95,19.49,16.99]
 //on click will pass values to detail card and open it and set details to true
 export default function Card(props){
    let choices=props.clothes===clothes[0]?hats:props.clothes===clothes[1]?shirts:pants
-   const [details,setDetails]=useState(false)
+   const [details,setDetails]=useState([false,'',0.0])
 
    function generatePics(){
-      return(<>{choices.map((choice,i)=>(
-         <><div className={details===false?"hideDetailCard"+(i+1):"showDetailCard"+(i+1)}>
-            <DetailCard key={i} image={<img key={choices+i} className={"cardPic Pic"+i} src={path+props.clothes.toLowerCase()+"/"+choice+".png"} alt={i}></img>} price={prices[i]} details={details} />
-            </div>
-         <img key={choices+i} className={"cardPic Pic"+i} src={path+props.clothes.toLowerCase()+"/"+choice+".png"} onClick={()=>setDetails(true) && console.log(details)} alt={i}></img></>
-   ))}</>)}
+      return(<>{choices.map((choice,i)=>(<img key={choices+i} className={"cardPic Pic"+i} src={path+props.clothes.toLowerCase()+"/"+choice+".png"} onClick={()=>setDetails([true,path+props.clothes.toLowerCase()+"/"+choice+".png",choice.replaceAll("-"," "),prices[i]]) && console.log(details)} alt={i}></img>))}</>)}
    function generatePrices(){
-      return(<>{prices.map((price,i)=>(
-         <h6 key={price+i} className={"Price"+i} onClick={()=>setDetails(true)}>{price}</h6>
-   ))}</>)}
-   return(
-   <>
-      <div className="Grid Card">
+      return(<>{prices.map((price,i)=>(<h6 key={price+i} className={"Price"+i} onClick={()=>setDetails(true)}>{price}</h6>))}</>)}
+   return(<>
+         <div className={details[0]===false?"hideDetailCard":"showDetailCard"}>
+            <DetailCard bool={details[0]} path={details[1]} clothes={details[2]} price={details[3]} />
+         </div>
+   <div className="Grid Card">
          
-      {/* <DetailCard/> */}
          <h5 className="Title">{"Newest "+props.clothes}</h5>
          {generatePics()}
          {generatePrices()}
--- src/pages/shopping/components/detail_card.jsx
+++ src/pages/shopping/components/detail_card.jsx
@@ -2,10 +2,14 @@ import React from "react"
 import "./detail_card.css"
 
 export default function details(props){
+   
    return(
    <>
       <div className="detailCard">
-         <h5 className="exit" onClick={()=>console.log(props.details)}>X</h5>
+         <img className="zoomImage" src={props.path} alt={props.clothes}></img>
+         <h4 className="title">{props.clothes}</h4><h6 className="price">{props.price}</h6>
+         <button className="button">Buy now</button>
+         <h5 className="exit" onClick={()=>console.log(props.path)}>X</h5>
       </div>
    </>)
 }
