e0e4ef6c14fbd424a159568dbd2f6a39784ef197
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2023-07-04 20:01:38 -0500

    adding minor fixes for shopping page mobile ui

--- src/pages/shopping/components/detail_card.jsx
+++ src/pages/shopping/components/detail_card.jsx
@@ -1,25 +1,22 @@
 import React, { Component } from "react"
 import "./detail_card.css"
-
+//TODO: fix css for mobile
 class detailCard extends Component{
    constructor(props){
       super(props)
-      this.closeWindow=this.closeWindow.bind(this)
+      this.closeWindow.bind(this)
    }
    closeWindow(){
       this.props.setBool(false)
    }
+   
    render(){
-      function filterInfo(word){
-         word.replace("-"," ")
-         return word
-      }
-
       return(
       <>
+         <br/>
          <div className="detailCard">
             <img className="zoomImage" src={this.props.path} alt={this.props.clothes}></img>
-            <h4 className="title">{filterInfo(this.props.clothes)}</h4><h6 className="price">{this.props.price}</h6>
+            <h4 className="title">{this.props.clothes}</h4><h6 className="price">{this.props.price}</h6>
             <button className="button">Buy now</button>
             <h5 className="exit" onClick={()=>this.closeWindow()}>X</h5>
          </div>
