8e287f403eb5c08a8dc7b81a12a7bb229276005e
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2023-07-05 12:30:23 -0500

    split the price into an array and decorated it so that now the cent amount is a smaller size than the dollar amount

--- src/pages/shopping/components/detail_card.jsx
+++ src/pages/shopping/components/detail_card.jsx
@@ -4,6 +4,9 @@ import "./detail_card.css"
 class detailCard extends Component{
    constructor(props){
       super(props)
+      this.state={
+         price:(this.props.price+"").split(".")
+      }
       this.closeWindow.bind(this)
    }
    closeWindow(){
@@ -15,7 +18,7 @@ class detailCard extends Component{
          <div className="detailBackground"></div>
          <div className="detailCard">
             <img className="zoomImage" src={this.props.path} alt={this.props.clothes}></img>
-            <h4 className="title">{this.props.clothes}</h4><h6 className="price">${this.props.price}</h6>
+            <h4 className="title">{this.props.clothes}</h4><h6 className="price">$<span className="largePrice">{this.state.price[0]}</span><span className="smallPrice">{this.state.price[1]}</span><br/><span className="inStock">In Stock</span></h6>
             <button className="button">Add to Cart</button>
             <h5 className="exit" onClick={()=>this.closeWindow()}>X</h5>
          </div>
