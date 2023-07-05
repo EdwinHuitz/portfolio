7bc8fb3f94ca6453607f93a74c84a1d591e5ccec
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2023-07-04 22:13:58 -0500

    fixed the navigation menu so that it resizes correctly for mobile

--- src/pages/shopping/shopping.jsx
+++ src/pages/shopping/shopping.jsx
@@ -1,16 +1,22 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './shopping.css'
 import Card from './components/card.jsx'
-//TODO: move shopping cart so it remains in view on mobile screen
-//TODO: create a function for setScreen to update window size to current window size whenever user changes window size 
+//TODO: animate the shopping cart and add a number to it whenever the user clicks the buy button 
 export default function Shopping(){
-   const [screen,setScreen]=useState(window.innerWidth)
    return(
    <>
       <div className="sApp">
          <nav className="navHead">
-            <h4 className="brand" onClick={()=>setScreen(window.innerWidth)}>Fashion Front</h4>
-            {screen>800?<><h4 className="links">Hats</h4><h4 className="links">Scarves</h4><h4 className="links">Shirts</h4><h4 className="links">Belts</h4><h4 className="links">Pants</h4><h4 className="links">Shoes</h4><i className="bx bx-cart shoppingCart"></i></>:<><h4 className="links">Clothes</h4><h4 className="links">Accessories</h4><i className="bx bx-cart shoppingCart"></i></>}
+            <h4 className="brand">Fashion Front</h4>
+            <h4 className="larger shoppingLinks">Hats</h4>
+            <h4 className="larger shoppingLinks">Scarves</h4>
+            <h4 className="larger shoppingLinks">Shirts</h4>
+            <h4 className="larger shoppingLinks">Belts</h4>
+            <h4 className="larger shoppingLinks">Pants</h4>
+            <h4 className="larger shoppingLinks">Shoes</h4>
+            <h4 className="smaller shoppingLinks">Clothes</h4>
+            <h4 className="smaller shoppingLinks">Accessories</h4>
+            <i className="bx bx-cart shoppingCart"></i>
          </nav>
          <div className="mainDisplay">
             <Card clothes="Hats"/>
