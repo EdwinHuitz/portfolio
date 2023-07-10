4ca62b555f0d42b3d9430dc275c2ecd3fbac9718
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2023-07-10 00:56:10 -0500

    adding a current month function with all 12 months to the localtime script

--- src/plugins/localTime.js
+++ src/plugins/localTime.js
@@ -25,22 +25,38 @@ function weekDay(){
 }
 function currentDate(){
   let date= d.getDate().toString()
-      let n
-      if(date.length===2){
-        date==="11"?n=date:n=date[1]
-      }
-      else{
-         n=date[0]
-      }
-      switch(n){
-         case "1":date=date+"st"
-         break
-         case "2":date=date+"nd"
-         break
-         case "3":date=date+"rd"
-         break
-         default:date=date+"th"
-      }
-      return date
+  let n
+  if(date.length===2){
+    date==="11"?n=date:n=date[1]
+  }
+  else{
+      n=date[0]
+  }
+  switch(n){
+      case "1":date=date+"st";break
+      case "2":date=date+"nd";break
+      case "3":date=date+"rd";break
+      default:date=date+"th"
+  }
+  return date
 }
-export{weekDay,currentTime,currentDate}
+function currentMonth(){
+  let month= d.getMonth()
+  switch(month){
+    case 1:month="January";break
+    case 2:month="February";break
+    case 3:month="March";break
+    case 4:month="April";break
+    case 5:month="May";break
+    case 6:month="June";break
+    case 7:month="July";break
+    case 8:month="August";break
+    case 9:month="September";break
+    case 10:month="October";break
+    case 11:month="November";break
+    case 12:month="December";break
+    default:month="Mystery Month"
+  }
+  return month
+}
+export{weekDay,currentMonth,currentTime,currentDate}
