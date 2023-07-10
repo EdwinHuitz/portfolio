e5b3241ae5c03bcfb1f269227e1618690463382e
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2023-07-10 16:43:49 -0500

    fixed the correct checked todo items not remaining checked after an item was deleted

--- src/pages/todo/todo.jsx
+++ src/pages/todo/todo.jsx
@@ -1,7 +1,7 @@
 import { Component } from 'react'
 import './todo.css'
 import { weekDay,currentMonth,currentDate } from '../../plugins/localTime.js'
-//TODO:add styling and fix checkbox not sticking to correct item when deletions happen. Also add more features
+//TODO:fix checkboxes not updating as soon as checked and also add more features
 class Todo extends Component{
    constructor(props){
       super(props)
@@ -10,7 +10,6 @@ class Todo extends Component{
          list:[],
          edit:"",
          key:0,
-         tasks:[],
       }
    }
    updateInput(text,int){
@@ -22,15 +21,13 @@ class Todo extends Component{
          const userInput = {
             id:Math.random(),
             value:this.state.input,
+            check:false,
          }
          const list = [...this.state.list]
-         const tasks = [...this.state.tasks]
          list.push(userInput)
-         tasks.push([0,userInput.id])
          //reset state for next item
          this.setState({
             list:list,
-            tasks:tasks,
             input:"",
             edit:"",
             key:0,
@@ -40,34 +37,27 @@ class Todo extends Component{
    handleSubmit(e){
       e.preventDefault()
    }
-   removeInput(key,i){
+   removeInput(key){
       const list = [...this.state.list]
-      const tasks = [...this.state.tasks]
       const updatedList = list.filter((item)=>item.id!==key)
-      tasks[i][0]=0
-      this.setState({tasks:tasks})
-      const updatedTasks = tasks.filter((task)=>task.id!==key)
-      console.log(i)
-      console.log("tasks: "+tasks)
-      console.log("updated tasks: "+updatedTasks)
-      this.setState({list:updatedList,tasks:updatedTasks,})
+      this.setState({list:updatedList,})
    }
    checkTask(i){
-      const tasks = [...this.state.tasks]
+      const tasks = [...this.state.list]
       const updatedTasks = tasks
-      updatedTasks[i][0]===0?updatedTasks[i][0]=1:updatedTasks[i][0]=0
-      this.setState({tasks:updatedTasks,})
+      updatedTasks[i][2]===false?updatedTasks[i][2]=true:updatedTasks[i][2]=false
+      this.setState({list:updatedTasks,})
    }
    showInput(item,key,i){
       return(<>
-         {(this.state.tasks[i][0]===1)?
-         <input name="checkBox" defaultValue="true" className="checkBox" type="checkbox" checked onChange={()=>this.checkTask(i)}></input>
+         {(this.state.list[i][2]===true)?
+         <input name="checkBox" defaultValue="" className="checkBox" type="checkbox" checked onChange={()=>this.checkTask(i)}></input>
          :<input name="checkBox" defaultValue="" className="checkBox" type="checkbox" onChange={()=>this.checkTask(i)}></input>}
-         <span className={this.state.tasks[i][0]===1?"struck todoText":"todoText"}>{item}</span>
+         <span className={this.state.list[i][2]===true?"struck todoText":"todoText"}>{item}</span>
          <button className="todoBtns editBtn" onClick={()=>this.setState({key:key,})}>
             <i className="bx bx-edit"></i>
          </button>
-         <button className="todoBtns trashBtn" onClick={()=>this.removeInput(key,i)}>
+         <button className="todoBtns trashBtn" onClick={()=>this.removeInput(key)}>
             <i className="bx bx-trash"></i>
          </button>
          </>)
@@ -81,7 +71,7 @@ class Todo extends Component{
          <button className="todoBtns saveBtn" onClick={()=>this.editInput(this.state.edit,i)}>
             <i className="bx bx-save"></i>
          </button>
-         <button className="todoBtns trashBtn" onClick={()=>this.removeInput(key,i)}>
+         <button className="todoBtns trashBtn" onClick={()=>this.removeInput(key)}>
             <i className="bx bx-trash"></i>
          </button>
          </>)
