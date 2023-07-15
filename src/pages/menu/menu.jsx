35d70c16448e5be63af5678c1e1cabaf2ca06b1d
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2023-07-14 19:44:10 -0500

    added unique prices for each item and small changes to menu page

--- src/pages/menu/menu.jsx
+++ src/pages/menu/menu.jsx
@@ -4,12 +4,12 @@ import './menu.css'
 export default function Menu(){
    const [cat,setCat]=useState(4)
 
-   let foodArray=[[0,"Pancakes"],[0,"Waffles"],[0,"Omelette"],[0,"Biscuits & Gravy"],[0,"Eggs Benedict"],[0,"Hashbrown"],[1,"Loaded Fries"],[1,"Bacon Cheeseburger"],[1,"Reuben Sandwich"],[2,"Milkshake"],[2,"Iced-Tea"],[2,"Coffee"]];
+   let foodArray=[[0,"Pancakes",9.99],[0,"Waffles",9.99],[0,"Omelette",13.95],[0,"Biscuits & Gravy",5.95],[0,"Eggs Benedict",6.75],[0,"Hashbrown",3.49],[1,"Loaded Fries",4.95],[1,"Bacon Cheeseburger",11.95],[1,"Reuben Sandwich",12.49],[2,"Milkshake",6.49],[2,"Iced-Tea",4.95],[2,"Coffee",3.49]];
    //array of numbers that is the total number of food items
    let numbers = [...Array(12).keys()]
    let path="assets/img/food/"
 
-   function menuItems(foodType,foodName){
+   function menuItems(foodType,foodName,foodPrice){
       let newName=''
       let newPath=''
       if(foodName.indexOf(' ')>=0){
@@ -23,7 +23,7 @@ export default function Menu(){
                <div className="menuItem" id={foodType} style={{display:foodType===cat?'flex':cat===3?'flex':'none'}}>
                      <img src={newPath} alt="" className="foodPic"></img>
                      <div className="foodDetail">
-                        <span className="foodText"><b>{foodName}</b> <b className="foodPrice">$15</b></span>
+                        <span className="foodText"><b>{foodName}</b> <b className="foodPrice">${foodPrice}</b></span>
                         <hr></hr>
                         <p>{foodName} are made of {foodName} mix which is made of {foodName} mix which is made of {foodName} mix which is made of {foodName}!</p>
                      </div>
@@ -66,15 +66,19 @@ export default function Menu(){
    <>
       <div className="menuWrap">
          <div className="menuNav">
-            <h1 className="title">Sunny's Diner</h1>
+            <h1 className="title" onClick={()=>setCat(4)}>Sunny's Diner</h1>
             <hr/>
-            <h3 className="tabs"><button onClick={()=>setCat(3)}>All</button><button onClick={()=>setCat(0)}>Breakfast</button><button onClick={()=>setCat(1)}>Lunch</button><button onClick={()=>setCat(2)}>Drinks</button>
+            <h3 className="tabs">
+               <button onClick={()=>setCat(3)} className={cat===3?"activeTabs":""}>All</button>
+               <button onClick={()=>setCat(0)} className={cat===0?"activeTabs":""}>Breakfast</button>
+               <button onClick={()=>setCat(1)} className={cat===1?"activeTabs":""}>Lunch</button>
+               <button onClick={()=>setCat(2)} className={cat===2?"activeTabs":""}>Drinks</button>
             </h3>
          </div>
          <div className="menuList">
             {foodTypes()}
             {numbers.map((num)=>(
-               menuItems(foodArray[num][0],foodArray[num][1])
+               menuItems(foodArray[num][0],foodArray[num][1],foodArray[num][2])
             ))}
          </div>
       </div>
