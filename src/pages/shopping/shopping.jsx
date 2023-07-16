66a2792e05ba89bbdedc0e7315bb545f833d0bba
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2023-07-16 04:47:34 -0500

    optimizing shopping page navigation and body with bootstrap and redoing css page with some class removals and additions

--- src/pages/shopping/shopping.jsx
+++ src/pages/shopping/shopping.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import './shopping.css'
 import Card from './components/card.jsx'
 //TODO: animate the shopping cart and add a number to it whenever the user clicks the buy button 
@@ -6,25 +5,42 @@ export default function Shopping(){
    return(
    <>
       <div className="sApp">
-         <nav className="navHead">
-            <h4 className="brand">Fashion Front</h4>
-            <h4 className="larger shoppingLinks">Hats</h4>
-            <h4 className="larger shoppingLinks">Scarves</h4>
-            <h4 className="larger shoppingLinks">Shirts</h4>
-            <h4 className="larger shoppingLinks">Belts</h4>
-            <h4 className="larger shoppingLinks">Pants</h4>
-            <h4 className="larger shoppingLinks">Shoes</h4>
-            <h4 className="smaller shoppingLinks">Clothes</h4>
-            <h4 className="smaller shoppingLinks">Accessories</h4>
-            <i className="bx bx-cart shoppingCart"></i>
+         <nav className="navbar fixed-top navbar-expand-lg" style={{backgroundColor:"#131921",color:"silver"}}>
+            <div className="container-fluid">
+               <div className="collapse navbar-collapse">
+                  <a className="navbar-brand brand" href="/#shopping">Fashion Front</a>
+                  <ul className="d-flex justify-content-evenly align-content-center navbar-nav w-100 p-3">
+                     <li className="nav-item"><button className="navLinkBtn">Hats</button></li>
+                     <li className="nav-item"><button className="navLinkBtn">Scarves</button></li>
+                     <li className="nav-item"><button className="navLinkBtn">Shirts</button></li>
+                     <li className="nav-item"><button className="navLinkBtn">Belts</button></li>
+                     <li className="nav-item"><button className="navLinkBtn">Pants</button></li>
+                     <li className="nav-item"><button className="navLinkBtn">Shoes</button></li>
+                     <li className="nav-item"><button className="navLinkBtn bx bx-cart shoppingCart" style={{fontSize:"1.5em",paddingLeft:"8px",paddingRight:"8px",margin:"0"}}></button></li>
+                  </ul>
+               </div>
+               <div className="smaller w-100 p-3">
+                  <ul className="d-flex justify-content-evenly">
+                     <li className="nav-item"><button className="navLinkBtn">Clothes</button></li>
+                     <li className="nav-item"><button className="navLinkBtn">Accessories</button></li>
+                     <li className="nav-item"><button className="navLinkBtn bx bx-cart shoppingCart" style={{fontSize:"1.5em",paddingLeft:"8px",paddingRight:"8px",margin:"0"}}></button></li>
+                  </ul>
+               </div>
+            </div>
          </nav>
          <div className="mainDisplay">
+            <div className="cardRow d-flex justify-content-evenly">
             <Card clothes="Hats"/>
             <Card clothes="Scarves"/>
+            </div>
+            <div className="cardRow d-flex justify-content-evenly">
             <Card clothes="Shirts"/>
             <Card clothes="Belts"/>
+            </div>
+            <div className="cardRow d-flex justify-content-evenly">
             <Card clothes="Pants"/>
             <Card clothes="Shoes"/>
+            </div>
          </div>
       </div>
    </>)
