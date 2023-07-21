7c35b35959e46b17af099fcd9673d75b81464996
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2023-07-21 16:01:05 -0500

    fixing bug in checkCart function and moving hr tag in shopping_cart_card

--- src/pages/shopping/components/cards/shopping_cart_card.jsx
+++ src/pages/shopping/components/cards/shopping_cart_card.jsx
@@ -10,15 +10,34 @@ export default function ShoppingCart(props){
          return(<h1>This cart is currently empty...</h1>)
       }
       else{
-         content.map((item,i)=>(sum+=item[i].price))
-         return(content.map((clothes,i)=>(
-            <><span key={i} className="SCItem"><img className="SCImg" src={clothes[i].url} alt={clothes[i].title}></img>
-            <h4 className="SCTitle p-3">{clothes[i].title}</h4>
-            <h4 className="SCPrice p-3">${clothes[i].price}</h4>
-            <input className="SCInput p-3" type="number" defaultValue={clothes[i].amount} /></span>
-            </>
-            //console.log(clothes[0].title,clothes[0].price,clothes[0].url,clothes[0].amount)
-         )))
+         let cartContent=[]
+         let int=100
+         content.forEach(c => {
+            //console.log(c[0]["title"])
+            console.log(c.length)
+            if(c.length>1){
+               c.map((item)=>(sum+=item.price))
+               cartContent.push(c.map((clothes,i)=>(
+               <span key={i} className="SCItem">
+                  <img className="SCImg" src={clothes.url} alt={clothes.title}></img>
+                  <h4 className="SCTitle p-3">{clothes.title}</h4>
+                  <h4 className="SCPrice p-3">${clothes.price}</h4>
+                  <input className="SCInput p-3" type="number" defaultValue={clothes.amount} />
+               </span>
+               //console.log(clothes[0].title,clothes[0].price,clothes[0].url,clothes[0].amount)
+               )))
+            }else if(c.length===1){
+               sum+=c[0].price
+               cartContent.push(<span key={int} className="SCItem">
+               <img className="SCImg" src={c[0].url} alt={c[0].title}></img>
+               <h4 className="SCTitle p-3">{c[0].title}</h4>
+               <h4 className="SCPrice p-3">${c[0].price}</h4>
+               <input className="SCInput p-3" type="number" defaultValue={c[0].amount} />
+               </span>)
+               int++
+            }
+         })
+         return(cartContent)
       }
    }
    //splits the cent and dollar amount for the total price and makes sure the cents don't go under or over 2 decimals
@@ -34,7 +53,7 @@ export default function ShoppingCart(props){
    }
    return(<div className="d-flex flex-column justify-content-center align-items-center w-100 p-3">
    {checkCart()}
-   <hr className="w-100 p-1"/>
-   {content.length>0?<h4>Total Cost: ${splitSum(sum)[0]}.{splitSum(sum)[1]}</h4>:""}
+   
+   {content.length>0?<><hr className="w-100 p-1"/><h4>Total Cost: ${splitSum(sum)[0]}.{splitSum(sum)[1]}</h4></>:""}
    </div>)
 }
